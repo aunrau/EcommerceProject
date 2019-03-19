c219e8c0b0f3e473ac91656a17ad8a0d997b7660
Angela Unrau <angie_tora@hotmail.com>
2019-03-19 11:32:48 -0500

    Added the Carrierwave and minimagick gems to upload, store, and resize images.

--- DragonsRawrUs/app/controllers/products_controller.rb
+++ DragonsRawrUs/app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.order(:product_name)
+    # @products = Product.order('id DESC')
   end
 
   # GET /products/1
--- DragonsRawrUs/app/models/product.rb
+++ DragonsRawrUs/app/models/product.rb
@@ -4,4 +4,6 @@ class Product < ApplicationRecord
     has_many :orders, through: :products_orders
 
     validates :price, numericality: true
+
+    mount_uploader :image, ImageUploader
 end
