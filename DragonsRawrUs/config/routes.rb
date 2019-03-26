07103d387bc2af142b2b1366090ff22343c241b6
Angela Unrau <angie_tora@hotmail.com>
2019-03-26 08:50:38 -0500

    Added an about page.

--- DragonsRawrUs/config/routes.rb
+++ DragonsRawrUs/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pages
   devise_for :customers
    devise_scope :customer do
     get '/login' => 'devise/sessions#new'
--- DragonsRawrUs/db/schema.rb
+++ DragonsRawrUs/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_163304) do
+ActiveRecord::Schema.define(version: 2019_03_26_133011) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -73,6 +73,14 @@ ActiveRecord::Schema.define(version: 2019_03_25_163304) do
     t.string "order_details"
     t.integer "customer_id"
     t.integer "prod_order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
