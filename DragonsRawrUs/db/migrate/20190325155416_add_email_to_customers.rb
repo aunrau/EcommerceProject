d1878f17ac0733afae404981e514177f50fb3ee3
Angela Unrau <angie_tora@hotmail.com>
2019-04-09 12:08:00 -0500

    Deployed to Heroku.

--- DragonsRawrUs/db/migrate/20190325155416_add_email_to_customers.rb
+++ DragonsRawrUs/db/migrate/20190325155416_add_email_to_customers.rb
@@ -1,5 +1,5 @@
 class AddEmailToCustomers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :customers, :email, :string
-  end
+  # def change
+  #   add_column :customers, :email, :string
+  # end
 end
--- DragonsRawrUs/db/seeds.rb
+++ DragonsRawrUs/db/seeds.rb
@@ -29,4 +29,4 @@ OrderStatus.create! id: 2, name: "Placed"
 OrderStatus.create! id: 3, name: "Shipped"
 OrderStatus.create! id: 4, name: "Cancelled"
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'tora@hotmail.com', password: '^foqbvy#dsc5mvk', password_confirmation: '^foqbvy#dsc5mvk')
